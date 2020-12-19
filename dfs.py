f6bea72c7d5c2655bf5ce27f89328b2d58f4bb8d
Raghav Nauhria <raghavnauhria@Raghavs-MacBook-Pro.local>
2020-12-19 14:01:25 +0530

    Final Working Code

--- dfs.py
+++ dfs.py
@@ -1,23 +1,33 @@
 class dfs(AlgorithmBase):
     
     def execute(self):
+        # init
         start=self.start_nodes[0]
         goal=self.goal_nodes[0]
         queue,visited=self.get_list('open'),self.get_list('closed')
         queue.append(start)
         
+        # kernel
         while queue:
             self.alg_iteration_start()
+
             node = queue.pop(0)
             visited.append(node)
+
+            # check for GOAL node
             if node == goal:
                 visited.append(node)
                 self.found_goal=True
                 break
+            
+            # iterate over children
+            index = 0
             for neighbor in self.neighbors(node):
                 if neighbor not in visited and neighbor not in queue:
                     self.set_parent(neighbor,node)
-                    queue.insert(0, neighbor)
+                    queue.insert(index, neighbor)
+                    index = index + 1
+        
             self.alg_iteration_end()
             
         if self.found_goal:
