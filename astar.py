5c2e14e53f76e1e69594805276c55b66c2d83389
Raghav Nauhria <raghavnauhria@Raghavs-MacBook-Pro.local>
2020-12-20 21:10:24 +0530

    start_nodes and goal_nodes error resolved

--- astar.py
+++ astar.py
@@ -3,8 +3,8 @@ import numpy as np
 class astar(AlgorithmBase):
 
     def execute(self):
-        start=self.start
-        goal=self.goal
+        start=self.start_nodes[0]
+        goal=self.goal_nodes[0]
         queue,visited=self.get_list('open'),self.get_list('closed')
         queue.append(start)
         g={i:np.inf  for i in self.get_nodes()}
@@ -13,24 +13,32 @@ class astar(AlgorithmBase):
 
         while queue:
             self.alg_iteration_start()
+            
+            # first "filter" all in "f" which are present in "queue"
+            # then get the node with the minimum "f"-value
             node=min(filter(lambda x: x in queue,f),key=lambda x:f[x])
+            
             queue.remove(node)
             visited.append(node)
+            
             if node==goal:
                 visited.append(node)
                 self.found_goal=True
                 break
+            
             for neighbor in self.neighbors(node):
-                t_g=g[node]+self.get_edge_weight(node,neighbor)
-                if t_g<g[neighbor]:
+                t_g = g[node] + self.get_edge_weight(node,neighbor)
+                if t_g < g[neighbor]:
                     self.set_parent(neighbor,node)
-                    g[neighbor]=t_g
-                    f[neighbor]=g[neighbor]+self.heuristic(neighbor,goal,)*2
+                    g[neighbor] = t_g
+                    f[neighbor] = g[neighbor]+self.heuristic(neighbor,goal)*2
                     if neighbor not in queue:
                         queue.append(neighbor)
 
             self.alg_iteration_end()
 
-        if self.found_goal:self.genpath()
-        else: self.show_info('No path available')
+        if self.found_goal:
+            self.genpath()
+        else:
+            self.show_info('No path available')
         self.execute_end()
